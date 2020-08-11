d6b3ab32eed5c48b36bb83b3efd7495d716ec76b
Jay Hughes <hello@kaiserbear.co.uk>
2020-08-11 10:47:10 +0100

    Uploading theme.

---
+++ content/themes/cratedigs/assets/js/streaminfo.js
@@ -0,0 +1,27 @@
+var tday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+var tmonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+function GetClock() {
+
+    var d = new Date();
+    var nday = d.getDay(),
+        nmonth = d.getMonth(),
+        ndate = d.getDate();
+    var nhour = d.getHours(),
+        nmin = d.getMinutes();
+    if (nmin <= 9) nmin = "0" + nmin
+
+    var Clockbox = document.getElementById('clockbox');
+    if (Clockbox) {
+        Clockbox.innerHTML = "" + tday[nday] + ", " + tmonth[nmonth] + " " + ndate + " : " + nhour + ":" + nmin + "";
+        document.getElementById('schedule-' + tday[nday]).classList.add("active-day");
+    }
+}
+
+function IsLive() {
+    if ($(".cc_streaminfo:contains('LIVE')").length > 0) {
+        $('.audio-player').addClass('live');
+    } else if ($(".cc_streaminfo:contains('LIVE')").length == 0) {
+        $('.audio-player').removeClass('live');
+    }
+}
