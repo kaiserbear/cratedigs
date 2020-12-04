d5e7b6fe18e9a86c29d0c35e1b352a283dd75ebd
Jay Hughes <hello@kaiserbear.co.uk>
2020-12-04 14:14:29 +0000

    Updating the menu as that wasn't working.

--- content/themes/cratedigs/assets/js/audio-player.js
+++ content/themes/cratedigs/assets/js/audio-player.js
@@ -23,14 +23,22 @@ playBtn.addEventListener(
 
 const hamburgerMenu = document.getElementById("menu--btn");
 const navigation = document.getElementById("nav-menu");
+const $navigationItem = $('#nav-menu li a');
 
 function nav() {
+
+    console.log($navigationItem);
+
     hamburgerMenu.onclick = function(e) {
-        console.log('check');
         hamburgerMenu.classList.toggle("open");
         navigation.classList.toggle("open");
-
     }
+    $navigationItem.on("click", function() {
+        console.log('check');
+        navigation.classList.toggle("open");
+        hamburgerMenu.classList.toggle("open");
+    });
+
 }
 
 nav();
