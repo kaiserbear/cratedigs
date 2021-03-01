fe2e66fc4470fc36d152674476f96125bb22fea2
Jay Hughes <hello@kaiserbear.co.uk>
2021-03-01 23:31:00 +0000

    Pushing changes back - stats are being messed up.

--- content/themes/cratedigs/assets/js/audio-player.js
+++ content/themes/cratedigs/assets/js/audio-player.js
@@ -1,7 +1,7 @@
 const audioPlayer = document.querySelector(".audio-player");
 const audio = new Audio(
-    // "http://cratedigs.radioca.st/stream"
-    "https://visual.shoutca.st/stream/cratedigstemp"
+    "https://cratedigs.radioca.st/stream"
+    // "https://visual.shoutca.st/stream/cratedigstemp"
 );
 
 //toggle between playing and pausing on button click
